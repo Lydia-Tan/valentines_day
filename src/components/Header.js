6d1c119249f6f7fc2c4b9f12594229fc06d394fb
Lydia Tan <lydia.sydia@gmail.com>
2021-02-13 02:49:39 -0500

    starting header file

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Header(){
+    return(
+        <div className = 'header-container'>
+            <h1>Hello! Happy V-day  ~name~ (for privacy's sake)-- oops I mean Valentine's Day ;) hahaha jk </h1>
+            <p>I thought to put my beep boop </p>
+
+        </div>
+    )
+}
+export default Header
